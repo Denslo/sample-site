407020bd301e424bafc843b7776a2f898a4e95da
Ran Klein <ranklein86@gmail.com>
2015-02-17 13:51:55 -0800

    end of g love for real

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-var jade = require('jade');
 var express = require('express');
 var app = express();
 
@@ -6,15 +5,7 @@ app.set('views', 'cloud/views');
 app.set('view engine', 'jade');
 app.use(express.bodyParser());
 
-var dataSrc = "cloud/data.js";
-
-if(process.env != undefined) {
-    if (process.env.IS_LOCAL) {
-        dataSrc = "./cloud/data.js";
-    }
-}
-
-var data = require(dataSrc);
+var data = require('./code/data/data.js');
 
 app.get('/', function(req, res){
     res.send('hi');
@@ -23,34 +14,20 @@ app.get('/', function(req, res){
 app.get('/website', function(req, res){
 
     var pageId = randomInt(data.results.length);
-    var reqId = 0;
+    var reqId = -1;
 
-    if(req.query.id !== undefined) {
+    if(req.query.id !== undefined)
         pageId = req.query.id % data.results.length;
-    }
-    var result = data.results[pageId];
 
-    if(req.query.recorder !== undefined) {
+    if(req.query.recorder !== undefined)
         reqId = parseInt(req.query.recorder);
 
-        if(reqId !== NaN){
-            if(reqId < 0){
-                result.head = undefined;
-                result.tail = getRecJade(reqId);
-            }
-            if(reqId > 0){
-                result.tail = undefined;
-                result.head = "<h1> aaaaaa</h1>>";//getRecJade(reqId);
-            }
-        }
-    }
+    var result = data.results[pageId];
 
     result.title = 'website';
     result.pageID = pageId;
     result.reqId = reqId;
 
-    console.log(result.head);
-    console.log(result.tail);
     res.render('website', result);
 });
 
@@ -58,43 +35,10 @@ app.get('/spa', function(req, res){
     res.render('spa', { title: 'spa' });
 });
 
-app.listen(3000);
-
 function randomInt(max)
 {
     return Math.floor(Math.random()*(max+1));
 }
 
-function getRecJade(recID){
 
-    var jadeName;
-
-    switch (recID){
-        case 1:
-            jadeName = 'inspectlet';
-            break;
-        case 2:
-            jadeName = 'navilytics';
-            break;
-        case 3:
-            jadeName = 'luckyorange';
-            break;
-        case -1:
-            jadeName = 'sessioncam';
-            break;
-        case -2:
-            jadeName = 'mouseflow';
-            break;
-        case -3:
-            jadeName = 'mousestats';
-            break;
-        case -4:
-            jadeName = 'clicky';
-            break;
-        default:
-            jadeName = 'empty';
-    }
-
-
-    return jadeName;
-}
+app.listen(3000);
