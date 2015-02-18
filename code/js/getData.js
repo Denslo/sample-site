5369b4ab4ef5c578a477e403974e0c214dc1686b
Ran Klein <ranklein86@gmail.com>
2015-02-17 17:24:24 -0800

    working on spa

--- code/js/getData.js
+++ code/js/getData.js
@@ -1,3 +1,28 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+    $.getJSON('/data',function (data){
+        window.allData = data;
+        initPage();
+    })
+
 });
+
+function initPage(){
+    var items = [];
+    $.each( window.allData.results, function( index, jsonData ) {
+        if(index === 0){
+            items.push( "<div class='page'>" );
+        } else {
+            items.push( "<div class='page hide'>" );
+        }
+
+        items.push( "<img src="+ jsonData.logo +">" );
+        items.push( "<h1>"+ jsonData.name +"</h1>" );
+        items.push( "<h2>Company Summery -</h2>" );
+        items.push( "<p>"+ jsonData.promise +"</p>" );
+        items.push( "<p>"+ jsonData.summery +"</p>" );
+
+        items.push( "</div>" );
+    });
+    $( "body" ).append(items.join(""));
+}
